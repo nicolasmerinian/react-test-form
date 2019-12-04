9e2127a3da688f2bb9e45a385b8c530bab2a6f3f
Nicolas MERINIAN <nicolasmerinian>
2019-12-04 16:24:50 +0100

    rename header menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,13 +16,13 @@ const Header = () => {
         <ul className='navLinks'>
           <li>
             <NavLink to="/" isActive={checkActive}>Home</NavLink>
-            <NavLink to="/page1">Page1</NavLink>
-            <NavLink to="/page2">Page2</NavLink>
-            <NavLink to="/page3">Page3</NavLink>
-            <NavLink to="/page4">Page4</NavLink>
-            <NavLink to="/page5">Page5</NavLink>
-            <NavLink to="/page6">Page6</NavLink>
-            <NavLink to="/page7">Page7</NavLink>
+            <NavLink to="/page1">Gender</NavLink>
+            <NavLink to="/page2">Class</NavLink>
+            <NavLink to="/page3">Skin</NavLink>
+            <NavLink to="/page4">Eye</NavLink>
+            <NavLink to="/page5">Hair</NavLink>
+            <NavLink to="/page6">Clothes</NavLink>
+            <NavLink to="/page7">Name</NavLink>
             <NavLink to="/final">End</NavLink>
           </li>
         </ul>
